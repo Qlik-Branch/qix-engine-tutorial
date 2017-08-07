57bf13f6ee47049df2b193074dd48b6100b35f8d
John Bellizzi <jabellizzi406@gmail.com>
2017-08-06 22:19:34 -0400

    added interactive validation to chapter 104

--- src/js/104/104. Stay in Sync with the Engine.js
+++ src/js/104/104. Stay in Sync with the Engine.js
@@ -1,11 +1,17 @@
 import activateSidebar from '../lib/activate-sidebar.js';
 import aceEditor from '../lib/ace-editor.js';
 
+import '../../sass/104/104. Stay in Sync with the Engine.scss';
+
+import interactiveValidation from './interactive-validation.js';
+
 import selectRequest from './code-snippets/select-request.js';
 import selectResponse from './code-snippets/select-response.js';
 import getLayout from './code-snippets/get-layout.js';
 
 activateSidebar(4);
+
+interactiveValidation('.interactive-validation');
 
 aceEditor('select-request', 'json', selectRequest, 'Apply Selection');
 aceEditor('select-response', 'json', selectResponse, 'Get Response');
